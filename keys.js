b68da32df8b590538d244a366a318fa279776db6
Martin <martinreyes501@gmail.com>
2023-01-03 19:46:34 -0700

    login ya con la bd

--- keys.js
+++ keys.js
@@ -2,13 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'Martinreyes1',
-        database: 'wikiredit'
+        host: 'bg7bssmyg2uadxzfoes0-mysql.services.clever-cloud.com',
+        user: 'uyhwapycslfjzq6k',
+        password: 'CjicXlCoIkVfYpfzN9f9',
+        database: 'bg7bssmyg2uadxzfoes0'
     }
 };
-/*host: 'bg7bssmyg2uadxzfoes0-mysql.services.clever-cloud.com',
-user: 'uyhwapycslfjzq6k',
-password: 'CjicXlCoIkVfYpfzN9f9',
-database: 'bg7bssmyg2uadxzfoes0'*/ 
+/*'*/ 
