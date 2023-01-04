9199c61c8471c53a018d7c76a9430919381f5513
Martin <martinreyes501@gmail.com>
2023-01-03 19:40:04 -0700

    con el login

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
 const usuarioRoutes_1 = __importDefault(require("./routes/usuarioRoutes"));
 const comentarioRoutes_1 = __importDefault(require("./routes/comentarioRoutes"));
 const com_usuRoutes_1 = __importDefault(require("./routes/com-usuRoutes"));
+const photoRoutes_1 = __importDefault(require("./routes/photoRoutes"));
+const loginRoutes_1 = __importDefault(require("./routes/loginRoutes"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
@@ -23,6 +26,8 @@ class Server {
         this.app.use((0, cors_1.default)());
         this.app.use(express_1.default.json());
         this.app.use(express_1.default.urlencoded({ extended: false }));
+        //Para que pueda acceder a las fotos
+        this.app.use('/photo', express_1.default.static(path_1.default.resolve('photo')));
     }
     routes() {
         this.app.use('/', indexRoutes_1.default);
@@ -30,6 +35,8 @@ class Server {
         this.app.use('/api/usuarios', usuarioRoutes_1.default);
         this.app.use('/api/comentarios', comentarioRoutes_1.default);
         this.app.use('/api/comentario/usu/', com_usuRoutes_1.default);
+        this.app.use('/api/photo/', photoRoutes_1.default);
+        this.app.use('/api/login/', loginRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
