6af1b637e7a65a7180f9d481e3b2994fb9aea962
Martin <martinreyes501@gmail.com>
2023-01-04 06:31:25 -0700

    regresando a la bd chafa

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'mysql-103169-0.cloudclusters.net',
-        user: 'admin',
-        password: 'papafrita1',
-        database: 'wikiredit'
+        host: 'bg7bssmyg2uadxzfoes0-mysql.services.clever-cloud.com',
+        user: 'uyhwapycslfjzq6k',
+        password: 'CjicXlCoIkVfYpfzN9f9',
+        database: 'bg7bssmyg2uadxzfoes0'
     }
 };
 /*'*/ 
